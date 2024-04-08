154ffac516a475a7d332286e2bcdc482e145ee1f
Anne Salminen <amauran@gmail.com>
2024-04-08 15:58:25 +0300

    added comment

--- 100Days/Day 2/Debugging.py
+++ 100Days/Day 2/Debugging.py
@@ -12,6 +12,7 @@
 # e.g. print("Hello " + "world")
 # New lines can be created with a backslash and n.
 
+# the fixed code
 print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
