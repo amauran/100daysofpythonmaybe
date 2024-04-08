a8332a877944842703ac93071053b1d3e2566b63
Anne Salminen <amauran@gmail.com>
2024-04-08 15:55:44 +0300

    getting started and stuff :D

---
+++ 100Days/Day 1/HelloWorld.py
@@ -0,0 +1,25 @@
+# the classic :)
+print("Hello World")
+
+# using single quotes and double quotes with print
+# if you need to use double quotes for something inside the string,
+# use single quotes to mark where the string to be printed begins and ends
+print('She said: "Hello" and then left.')
+
+# the reverse works too
+print("She said: 'Hello' and then left.")
+
+# or alternatively, use the escape \ to mark which characters to ignore as code
+print("She said: \"Hello\" and then left.")
+
+# use \n for new line (enter/return)
+print("Hello World\nHello World\nHello World")
+
+# string concatenation — how strings are combined
+print("Hello" + "Anne")             # result is HelloAnne
+print("Hello" + " " + "Anne")       # result is Hello Anne
+print("Hello","Anne", sep=' ')      # same here but via a different method
+
+
+
+
